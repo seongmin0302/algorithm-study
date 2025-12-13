eeb486c500c95daf8bd3d3b35df05579ae5fa7c8
seongmin0302 <seongmin0302@naver.com>
2025-12-13 06:32:51 +0000

    public static int[] mergeSort(int[] array) 함수 구현-> 병합정렬 모두 구현완료

--- 입문/ch03_정렬_스택과큐_해쉬/_04_병합정렬.java
+++ 입문/ch03_정렬_스택과큐_해쉬/_04_병합정렬.java
@@ -3,6 +3,24 @@ package 입문.ch03_정렬_스택과큐_해쉬;
 import java.util.Arrays;
 
 public class _04_병합정렬 {
+
+    public static int[] mergeSort(int[] array) {
+        if (array.length <= 1) {
+            return array;
+        }
+        
+        int mid = array.length / 2;
+        int[] leftArray = Arrays.copyOfRange(array, 0, mid);
+        int[] rightArray = Arrays.copyOfRange(array, mid, array.length);
+        
+        // System.out.println(Arrays.toString(array));
+        // System.out.println(Arrays.toString(leftArray));
+        // System.out.println(Arrays.toString(rightArray));
+
+        return merge(mergeSort(leftArray),mergeSort(rightArray));
+    }
+
+
     public static int[] merge(int[] array1, int[] array2) {
         int[] result = new int[array1.length + array2.length];
         int resultIndex = 0;
@@ -37,5 +55,19 @@ public class _04_병합정렬 {
         int[] array_a = {1, 2, 3, 5};
         int[] array_b = {4, 6, 7, 8};
         System.out.println("정답 = "+Arrays.toString(merge(array_a, array_b)));
+
+        System.out.println();
+        System.out.println("public static int[] mergeSort(int[] array) test:");
+        int[] array = {5, 3, 2, 1, 6, 8, 7, 4};
+        System.out.println("정답 = "+Arrays.toString(mergeSort(array)));
+        
+        System.out.print("정답 = [-7, -1, 5, 6, 9, 10, 11, 40] / 현재 풀이 값 = ");
+        System.out.println(Arrays.toString(mergeSort(new int[]{-7, -1, 9, 40, 5, 6, 10, 11})));
+        
+        System.out.print("정답 = [-1, 2, 3, 5, 10, 40, 78, 100] / 현재 풀이 값 = ");
+        System.out.println(Arrays.toString(mergeSort(new int[]{-1, 2, 3, 5, 40, 10, 78, 100})));
+        
+        System.out.print("정답 = [-1, -1, 0, 1, 6, 9, 10] / 현재 풀이 값 = ");
+        System.out.println(Arrays.toString(mergeSort(new int[]{-1, -1, 0, 1, 6, 9, 10})));
     }
 }
