7bd6b91971488efef4e8f4389013e2700a993547
seongmin0302 <seongmin0302@naver.com>
2025-12-14 10:28:49 +0000

    주식가격 문제 - 큐로 풀이함

--- 입문/ch03_정렬_스택과큐_해쉬/_08_주식가격.java
+++ 입문/ch03_정렬_스택과큐_해쉬/_08_주식가격.java
@@ -1,36 +1,49 @@
 package 입문.ch03_정렬_스택과큐_해쉬;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class _08_주식가격 {
     public static void main(String[] args) {
         
         System.out.print("정답 = [4, 3, 1, 1, 0] / 현재 풀이 값 = ");
-        System.out.println(Arrays.toString(getPriceNotFallPeriods(new int[]{1, 2, 3, 2, 3})));
+        System.out.println(getPriceNotFallPeriods(new int[]{1, 2, 3, 2, 3}));
         
         System.out.print("정답 = [6, 2, 1, 3, 2, 1, 0] / 현재 풀이 값 = ");
-        System.out.println(Arrays.toString(getPriceNotFallPeriods(new int[]{3, 9, 9, 3, 5, 7, 2})));
+        System.out.println(getPriceNotFallPeriods(new int[]{3, 9, 9, 3, 5, 7, 2}));
         
         System.out.print("정답 = [6, 1, 4, 3, 1, 1, 0] / 현재 풀이 값 = ");
-        System.out.println(Arrays.toString(getPriceNotFallPeriods(new int[]{1, 5, 3, 6, 7, 6, 5})));
+        System.out.println(getPriceNotFallPeriods(new int[]{1, 5, 3, 6, 7, 6, 5}));
     }
 
-    public static int[] getPriceNotFallPeriods(int[] prices) {
-        int[] result=new int[prices.length];
-        for(int i=0;i<prices.length;i++) {
-            int priceNotFallPeriod=0;
-            for(int j=i+1;j<prices.length;j++) {
-                if(prices[i]<=prices[j]) {
+    public static List<Integer> getPriceNotFallPeriods(int[] prices) {
+        List<Integer> result = new ArrayList<>();
+        Queue<Integer> pricesQueue = new LinkedList<>();
+        
+        for (int price : prices) {
+            pricesQueue.offer(price);
+        }
+        
+        while (!pricesQueue.isEmpty()) { 
+            int priceNotFallPeriod = 0;
+            int currentPrice = pricesQueue.poll(); 
+            
+            Queue<Integer> tempQueue = new LinkedList<>(pricesQueue);
+            
+            for (int nextPrice : tempQueue) {
+                if (currentPrice <= nextPrice) {
                     priceNotFallPeriod++;
                 } else {
                     priceNotFallPeriod++;
                     break;
                 }
             }
-
-            result[i]=priceNotFallPeriod;
+            
+            result.add(priceNotFallPeriod);
         }
-
         return result;
     }
 
