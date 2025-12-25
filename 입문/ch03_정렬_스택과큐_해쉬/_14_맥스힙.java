0f52e3ff605ecaeedaa785e544b3727db62eb1e0
seongmin0302 <seongmin0302@naver.com>
2025-12-25 04:07:34 +0000

    맥스 힙의 원소 제거까지 모두 구현 완료

--- 입문/ch03_정렬_스택과큐_해쉬/_14_맥스힙.java
+++ 입문/ch03_정렬_스택과큐_해쉬/_14_맥스힙.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class _14_맥스힙 {
     public static void main(String[] args) {
+        System.out.println("맥스 힙에 원소 추가 test: ");
         MaxHeap maxHeap = new MaxHeap();
         maxHeap.insert(3);
         maxHeap.insert(4);
@@ -11,6 +12,26 @@ public class _14_맥스힙 {
         maxHeap.insert(9);
         System.out.print("[null, 9, 4, 2, 3] 가 출력되어야 합니다!-> ");
         System.out.println(maxHeap.items); 
+
+
+        System.out.println();
+        System.out.println("맥스 힙의 원소 제거 test: ");
+        MaxHeap maxHeap2 = new MaxHeap();
+        maxHeap2.insert(8);
+        maxHeap2.insert(6);
+        maxHeap2.insert(7);
+        maxHeap2.insert(2);
+        maxHeap2.insert(5);
+        maxHeap2.insert(4);
+
+        System.out.print("[null, 8, 6, 7, 2, 5, 4] 가 출력되어야 합니다!-> ");
+        System.out.println(maxHeap2.items);
+       
+        System.out.print("8 을 반환해야 합니다!->");
+        System.out.println(maxHeap2.delete()); 
+       
+        System.out.print("[null, 7, 6, 4, 2, 5] 가 출력되어야 합니다!-> ");
+        System.out.println(maxHeap2.items);
     }
 }
 
@@ -40,5 +61,41 @@ class MaxHeap {
                 break;
             }
         }
+    }
+
+    public int delete() {
+        // Swap root with last element
+        int temp = this.items.get(1);
+        this.items.set(1, this.items.get(this.items.size() - 1));
+        this.items.set(this.items.size() - 1, temp);
+        
+        int prevMax = this.items.remove(this.items.size() - 1); // 8
+        int curIndex = 1;
+
+        while (curIndex <= this.items.size() - 1) {
+            int leftChildIndex = curIndex * 2;
+            int rightChildIndex = curIndex * 2 + 1;
+            int maxIndex = curIndex;
+
+            if (leftChildIndex <= this.items.size() - 1 && this.items.get(leftChildIndex) > this.items.get(maxIndex)) {
+                maxIndex = leftChildIndex;
+            }
+
+            if (rightChildIndex <= this.items.size() - 1 && this.items.get(rightChildIndex) > this.items.get(maxIndex)) {
+                maxIndex = rightChildIndex;
+            }
+
+            if (maxIndex == curIndex) {
+                break;
+            }
+
+            // Swap elements
+            temp = this.items.get(curIndex);
+            this.items.set(curIndex, this.items.get(maxIndex));
+            this.items.set(maxIndex, temp);
+            curIndex = maxIndex;
+        }
+
+        return prevMax; // 8
     }
 }
